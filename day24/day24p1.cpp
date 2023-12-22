0a53aa50bbbb7df88981909648611e1d740ac68f
Mladen Stupar <cupostv@gmail.com>
2023-12-22 06:55:05 +0100

    Added Day 24

--- day24/day24p1.cpp
+++ day24/day24p1.cpp
@@ -1,6 +1,80 @@
 #include "../helpers/includes.hpp"
 
+// y1 = m1*x1 + c1
+// y2 = m2*x2 + c2
+// c1 = y1 - m1 * x1
+// c2 = y2 - m2 * x2
+
+// First equation
+// m1x + c1 = m2x + c2
+// m1x - m2x = c2 - c1
+// x = (c2 - c1) / (m1 - m2)
+// x = (y2 - m2x2 - y1 + m1x1) / (m1 - m2)
+// Second equation
+// y = m1 * (c2 - c1) / (m1 - m2) + c1
+// y = m1 * newX + y1 - m1 * x1
+// m == slope
+
 #define INPUT "input.txt"
+
+struct Hailstone {
+    int64_t positionX;
+    int64_t positionY;
+    int64_t positionZ;
+
+    int64_t velocityX;
+    int64_t velocityY;
+    int64_t velocityZ;
+
+    float getSlope() const {
+        return (float)velocityY / (float)velocityX;
+    }
+
+    bool isFuture(float x, float y) const {
+        if (velocityX < 0 && positionX < x) return false;
+        if (velocityX > 0 && positionX > x) return false;
+        if (velocityY < 0 && positionY < y) return false;
+        if (velocityY > 0 && positionY > y) return false;
+
+        return true;
+    }
+
+
+    bool intersects(const Hailstone &other, int64_t min, int64_t max) const {
+        auto slope = getSlope();
+        auto otherSlope = other.getSlope();
+
+        // Lines are parallel
+        if (slope == otherSlope) return false;
+
+        // Find intersection
+        // x = (y2 - m2x2 - y1 + m1x1) / (m1 - m2)
+        auto intersectX = (other.positionY - otherSlope * other.positionX - positionY + slope * positionX) / (slope - otherSlope);
+        // y = m1 * newX + y1 - m1 * x1
+        auto intersectY = slope * intersectX + positionY - slope * positionX;
+
+        if (intersectX < min || intersectX > max) return false;
+        if (intersectY < min || intersectY > max) return false;
+
+        return isFuture(intersectX, intersectY) && other.isFuture(intersectX, intersectY);
+    }
+
+    friend std::istream& operator >> (std::istream& is, Hailstone& p) {
+        is >> p.positionX;
+        is.ignore(3, ' ');
+        is >> p.positionY;
+        is.ignore(3, ' ');
+        is >> p.positionZ;
+        is.ignore(3, '@');
+        is >> p.velocityX;
+        is.ignore(3, ' ');
+        is >> p.velocityY;
+        is.ignore(3, ' ');
+        is >> p.velocityZ;
+        is.ignore(2);
+        return is;
+    }
+};
 
 int32_t main() {
 
@@ -9,7 +83,7 @@ int32_t main() {
 
     if (!input) return EXIT_FAILURE;
 
-    helper::Point startPos;
+    std::vector<Hailstone> hailstones;
 
     while (!input.eof()) {
 
@@ -17,7 +91,27 @@ int32_t main() {
 
         std::getline(input, inputRow);
         if (inputRow.empty()) break;
+
+        std::stringstream inputStream(inputRow);
+
+        Hailstone h;
+        inputStream >> h;
+
+        hailstones.push_back(h);
+
     }
+
+    int64_t intersections = 0;
+    for (int32_t i = 0; i < hailstones.size(); i++) {
+        for (int32_t j = i + 1; j < hailstones.size(); j++) {
+            // 7, 27 for test input
+            // 200000000000000, 400000000000000 for real input
+            if (hailstones[i].intersects(hailstones[j], 200000000000000, 400000000000000))
+                intersections++;
+        }
+    }
+
+    std::cout << intersections << std::endl;
 
     return EXIT_SUCCESS;
 }
---
+++ day24/day24p2.cpp
@@ -0,0 +1,57 @@
+#include "../helpers/includes.hpp"
+
+// fx(t) = x + t * vx
+
+// We have a system with multiple unknown
+// if we take 3 first points
+// x, vx, t0, t1, t2
+// x0 + t0 * vx0 = x + t0 * vx
+// x1 + t1 * vx1 = x + t1 * vx
+// x2 + t2 * vx2 = x + t2 * vx
+// If we do the same for y and z
+// We will get 9 equations and we can calculate x, y, z easily
+// Except it's too much to do manually
+// I could use Z3 library, but instead I will just calculate result with online equation solvers
+
+// First three points
+// 189484959431670, 401088290781515, 262795384692232 @ 95, -36, 39
+// 175716591307178, 163094456512341, 297355219841654 @ 160, 182, -56
+// 283402568811320, 363632912812075, 314435988531407 @ -6, 37, -10
+
+// (189484959431670 + t0 * 95 = x + t0 * vx)
+// (401088290781515 + t0 * -36 = y + t0 * vy)
+// (262795384692232 + t0 * 39 = z + t0 * vz)
+
+// (175716591307178 + t1 * 160 = x + t1 * vx)
+// (163094456512341 + t1 * 182 = y + t1 * vy)
+// (297355219841654 + t1 * -56 = z + t1 * vz)
+
+// (283402568811320 + t2 * -6 = x + t2 * vx)
+// (363632912812075 + t2 * 37 = y + t2 * vy)
+// (314435988531407 + t2 * -10 = z + t2 * vz)
+
+// Solve t0, t1, t2, x, y, z, vx, vy, vz
+// (x0 + t0 * vx0 = x + t0 * vx) => t0 * vx0 - t0 * vx = x - x0 => t0 = (x - x0) / (vx0 - vx)
+// (y0 + t0 * vy0 = y + t0 * vy) => t0 = (y - y0) / (vy0 - vy)
+// (z0 + t0 * vz0 = z + t0 * vz) =>
+// x = x0 + t0 * vx0 - t0 * vx
+// y = y0 + t0 * vy0 - t0 * vy
+// z = z0 + t0 * vz0 - t0 * vz
+
+// (x1 + t1 * vx1 = x + t1 * vx)
+// (y1 + t1 * vy1 = y + t1 * vy)
+// (z1 + t1 * vz1 = z + t1 * vz)
+
+// (x2 + t2 * vx2 = x + t2 * vx)
+// (y2 + t2 * vy2 = y + t2 * vy)
+// (z2 + t2 * vz2 = z + t2 * vz)
+
+#define INPUT "input.txt"
+
+int32_t main() {
+
+    // Thx https://quickmath.com/webMathematica3/quickmath/equations/solve/advanced.jsp#c=solve_solveequationsadvanced&v1=x%253D189484959431670%2Ba*95-a*d%250Ay%253D401088290781515%2Ba*-36-a*e%250Az%253D262795384692232%2520%2Ba*39-a*f%250Ax%253D175716591307178%2Bb*160-b*d%250Ay%253D163094456512341%2Bb*182-b*e%250Az%253D297355219841654%2Bb*-56-b*f%250Ax%253D283402568811320%2Bc*-6-c*d%250Ay%253D363632912812075%2Bc*37-c*e%250Az%253D314435988531407%2Bc*-10-c*f&v2=x%250Ay%250Az%250Aa%250Ab%250Ac%250Ad%250Ae%250Af <3
+    std::cout << std::fixed << 234382970331570LL + 100887864960615LL + 231102671115832LL << " :)" << std::endl;
+
+    return EXIT_SUCCESS;
+}
